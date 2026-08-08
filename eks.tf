32a5e870c06789d3aa61e2a305b5adf3a0d1f34c
root <root@ip-172-31-4-113.ec2.internal>
2026-08-08 21:33:40 +0000

    Add Terraform EKS infrastructure

---
+++ eks.tf
@@ -0,0 +1,32 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.37.2"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.32"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  eks_managed_node_groups = {
+    hrms_nodes = {
+      desired_size = 2
+      min_size     = 2
+      max_size     = 3
+
+      instance_types = ["t3.medium"]
+
+      capacity_type = "ON_DEMAND"
+    }
+  }
+
+  tags = {
+    Project   = "HRMS"
+    ManagedBy = "Terraform"
+  }
+}
